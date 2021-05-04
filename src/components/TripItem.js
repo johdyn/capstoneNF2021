67bedabf779e58cb1f395cf38c5c7304dbd49b70
Johan Dynnesen <johandynnesen@gmail.com>
2021-05-04 10:06:26 +0200

    Add functionality and basic styling to triplist and Addflight page, Trip items can be stored and removed from local storage

--- src/components/TripItem.js
+++ src/components/TripItem.js
@@ -1,9 +1,26 @@
-export default function TripItem({ date, travelType, amountCO2 }) {
+import "./TripItem.css";
+import { FaTrashAlt } from "react-icons/fa";
+import { removeItemFromLocalStorage } from "./tripStorage";
+
+export default function TripItem({
+  id,
+  date,
+  departure,
+  destination,
+  passengers,
+  onRemove,
+}) {
+  const showDate = date.slice(0, 10);
   return (
     <article className="trip-item">
-      <h2>{date}</h2>
-      <p classname="paragraph">{travelType}</p>
-      <p>{amountCO2}</p>
+      <h5 className="trip-item-date">{showDate}:</h5>
+      <div className="text-container">
+        <p>Passengers: {passengers}</p>
+        <p>Departure: {departure}</p>
+        <p className="paragraph">Destination: {destination}</p>
+        <p>CO2 Emission: </p>
+      </div>
+      <FaTrashAlt className="trip-item-delete" onClick={onRemove} />
     </article>
   );
 }
--- src/components/tripStorage.js
+++ src/components/tripStorage.js
@@ -1,3 +1,5 @@
+import generateID from "./generateID";
+
 export function getItemsFromLocalStorage() {
   const items = JSON.parse(localStorage.getItem("items")) || [];
 
@@ -6,7 +8,16 @@ export function getItemsFromLocalStorage() {
 
 export function addItemToLocalStorage(item) {
   const items = getItemsFromLocalStorage();
-
+  item.id = generateID();
   items.push(item);
   localStorage.setItem("items", JSON.stringify(items));
+}
+
+export function removeItemFromLocalStorage(itemID) {
+  const items = getItemsFromLocalStorage();
+  console.log(itemID);
+  const newItems = items.filter((item) => {
+    return item.id !== itemID;
+  });
+  localStorage.setItem("items", JSON.stringify(newItems));
 }
